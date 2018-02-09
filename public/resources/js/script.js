fd8eebc334dff9e501b09e2325d942c25739a883
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-02-09 16:24:40 +0530

    Added animations

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,9 +1,11 @@
+// CUSTOM SETTINGS for scroll reveal
+window.sr = ScrollReveal({reset: true});
+
 // Just a utility function 
 // Function for fetching the elements from DOM
 function getId(id) {
   return document.getElementById(id);
 }
-
 
 // Function for showing thw navbar after some scrolling
 window.onscroll = function() {
@@ -96,3 +98,70 @@ function showMessage(text, type) {
 function hideMessages() {
   getId('js-error-message-container').style.display = "none";
 }
+
+/* Scroll Reveal code starts from here */
+
+// For text written over slideshow
+sr.reveal('.image-text', {
+  duration: 1000,
+  origin: 'top',
+  distance: '40px'
+});
+
+sr.reveal('.sub-title', {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '20px'
+})
+
+// For introduction 
+sr.reveal('.intro-container', {
+  duration: 1000,
+  mobile: true,
+  origin: 'top',
+  move: '20px'
+});
+
+// Follow links
+sr.reveal('.follow-us-container', {
+  duration: 1500,
+  rotate: {x: 0, y: 100, z: 0},
+  origin: 'bottom',
+  distance: '15px'
+});
+
+// Headings on every section
+sr.reveal('.container__header', {
+  duration: 1500,
+  rotate: {x: 50, y: 0, z: 0}
+})
+
+// For foodcards
+sr.reveal('.container__body__foodimages', {
+  duration: 1500,
+  rotate: {x: 0, y: 100, z: 0}
+})
+
+// For foodcard badges
+sr.reveal('.badge', {
+  origin: 'bottom',
+  distance: '30px',
+  duration: 1000,
+  delay: 50
+}, 70);
+
+// For chef image in booking section
+sr.reveal('.chef-image', {
+  origin: 'left',
+  distance: '30px',
+  duration: 2000,
+  delay: 50
+})
+
+// For input fields in form
+sr.reveal('.booking-inp', {
+  origin: 'bottom',
+  duration: 2000,
+  distance: '15px',
+  delay: 20
+}, 250)
